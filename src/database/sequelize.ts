e4a40ad7cb6d17d25f0fb880372368fd8885c0e5
Gabriel Rabelo <rabelogabriel72@gmail.com>
2024-04-15 10:34:14 -0300

    chore: disable sequelize logging

--- src/database/sequelize.ts
+++ src/database/sequelize.ts
@@ -1,8 +1,6 @@
 import 'dotenv/config';
 import { Sequelize } from 'sequelize';
 
-console.log('database log', process.env.DATABASE_LOG);
-
 const sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: 'database.sqlite',
