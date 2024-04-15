bdd636251997145c81ffb2f68b9a057b24141bce
Gabriel Rabelo <rabelogabriel72@gmail.com>
2024-04-15 10:31:16 -0300

    chore: disable sequelize loggin

--- src/database/sequelize.ts
+++ src/database/sequelize.ts
@@ -1,8 +1,12 @@
+import 'dotenv/config';
 import { Sequelize } from 'sequelize';
+
+console.log('database log', process.env.DATABASE_LOG);
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: 'database.sqlite'
+  storage: 'database.sqlite',
+  logging: false
 });
 
 export default sequelize;
