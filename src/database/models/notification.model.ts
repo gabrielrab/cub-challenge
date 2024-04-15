f5c0bd8eb1f7c5d1fc48b80c07ba9a583fb0ec89
Gabriel Rabelo <rabelogabriel72@gmail.com>
2024-04-15 07:43:01 -0300

    feat: modeling notification model

--- src/database/models/notification.model.ts
+++ src/database/models/notification.model.ts
@@ -2,8 +2,27 @@ import { DataTypes } from 'sequelize';
 import sequelize from '../sequelize';
 
 const Notification = sequelize.define('Notification', {
-  name: {
+  channel: {
     type: DataTypes.STRING
+  },
+  to: {
+    type: DataTypes.STRING
+  },
+  body: {
+    type: DataTypes.STRING
+  },
+  externalId: {
+    type: DataTypes.STRING
+  },
+  status: {
+    type: DataTypes.STRING,
+    values: ['processing', 'rejected', 'sent', 'delivered', 'viewed']
+  },
+  statusTimestamp: {
+    type: DataTypes.DATE
+  },
+  version: {
+    type: DataTypes.INTEGER
   }
 });
 
